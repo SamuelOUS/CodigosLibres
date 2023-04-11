2a277f33b523a9b879174d5ed8b5f177f9d7d431
Samuel <110128889+SamuelOUS@users.noreply.github.com>
2023-04-11 08:48:05 -0500

    Update register_data_design.py

--- ProyectoRequisitos/Formulario/Registration/register_data_design.py
+++ ProyectoRequisitos/Formulario/Registration/register_data_design.py
@@ -38,6 +38,30 @@ class RegistrationWindowDesign:
         etiqueta_password.pack(fill=tk.X, padx=195, pady=5)
         self.password = ttk.Entry(frame_form_fill, font=('Times', 14))
         self.password.pack(fill=tk.X, padx=20, pady=10)
+        
+        etiqueta_apellidos = tk.Label(frame_form_fill, text="apellidos", font=('Times', 14), fg="black", bg="white",
+                                      anchor="w")
+        etiqueta_apellidos.pack(fill=tk.X, padx=195, pady=5)
+        self.apellidos = ttk.Entry(frame_form_fill, font=('Times', 14))
+        self.apellidos.pack(fill=tk.X, padx=20, pady=10)
+
+        etiqueta_ocupacion = tk.Label(frame_form_fill, text="ocupacion", font=('Times', 14), fg="black", bg="white",
+                                      anchor="w")
+        etiqueta_ocupacion.pack(fill=tk.X, padx=195, pady=5)
+        self.ocupacion = ttk.Entry(frame_form_fill, font=('Times', 14))
+        self.ocupacion.pack(fill=tk.X, padx=20, pady=10)
+
+        etiqueta_nacimiento = tk.Label(frame_form_fill, text="nacimiento", font=('Times', 14), fg="black", bg="white",
+                                       anchor="w")
+        etiqueta_nacimiento.pack(fill=tk.X, padx=195, pady=5)
+        self.nacimiento = ttk.Entry(frame_form_fill, font=('Times', 14))
+        self.nacimiento.pack(fill=tk.X, padx=20, pady=10)
+
+        etiqueta_correo = tk.Label(frame_form_fill, text="correo", font=('Times', 14), fg="black", bg="white",
+                                   anchor="w")
+        etiqueta_correo.pack(fill=tk.X, padx=195, pady=5)
+        self.correo = ttk.Entry(frame_form_fill, font=('Times', 14))
+        self.correo.pack(fill=tk.X, padx=20, pady=10)
 
         register = tk.Button(frame_form_fill, text="Registrar usuario", font=('Times', 15), bg="#556F8A", bd=0,fg="white", command=self.data)
         register.pack(fill=tk.X, padx=20, pady=20)
